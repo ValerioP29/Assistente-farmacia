8dcfdd2e4836f3beaa54e6be6387b05a2f0e0dee
Alberto Borelli <alberto.borelli@jungleteam.it>
2025-08-06 17:10:17 +0200

    Bug Fixing Notification

--- includes/footer.php
+++ includes/footer.php
@@ -19,8 +19,7 @@
         </div>
     </footer>
 
-    <!-- Bootstrap JS -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
     
     <!-- Chart.js -->
     <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.0/dist/chart.umd.min.js"></script>
